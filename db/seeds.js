baf2de34448ad2a500482cbdaeb719c8b16b7b01
dcamriff <driffelatl@gmail.com>
2018-01-04 16:35:52 -0500

    added test data to seeds

--- db/seeds.js
+++ db/seeds.js
@@ -18,3 +18,27 @@ mongoose.connection.on('error', (error) => {
     process.exit(-1)
 })
 
+// delete users then add fake test ones
+User.remove({}).then(() => {
+    const dianeRiffel = new User({
+        username: 'lil_diane',
+        email: 'diane@saturationproject.com',
+        firstName: 'Diane',
+        lastName: 'Cam-Riffel',
+        photoUrl: 'https://i.imgur.com/nbG1deE.png'
+    })
+
+    const riloKiley1 = new Poster({
+        title: 'Riley Kiley 2005 Tour',
+        story: 'Favorite Poster',
+        yearOfShow: 2005,
+        monthOfShow: 05,
+        dayOfShow: 01,
+        locationCity: 'Philadelphia',
+        imgUrl: 'https://i.imgur.com/xUNTDKM.jpg',
+        artist: 'Eleanor Grosch / Push Me Pull You Design',
+        mediumType: 'Hand Screenprinted on paper',
+        limtedEdition: true,
+        bands: 'Rilo Kiley'
+    })
+})
