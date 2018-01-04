26b03d9f1939739f1047dce3bc3697fabfb8ddb4
dcamriff <driffelatl@gmail.com>
2018-01-04 15:59:55 -0500

    added unique requirement to  UserSchema and added PosterSchema and BandSchema

--- db/schema.js
+++ db/schema.js
@@ -9,7 +9,8 @@ const UserSchema = new Schema(
     {
         username: {
             type: String,
-            required: [ true, 'Username is required!' ]
+            required: [ true, 'Username is required!' ],
+            unique: true
         },
         email: {
             type: String,
@@ -31,5 +32,48 @@ const UserSchema = new Schema(
     {
     timestamps: {},
     usePushEach: true
+    }
+)
+
+const PosterSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [ true, 'The poster title is required!' ]
+        },
+        story: {
+            type: String,
+            default: "Tell a fun memory about how/when this poster was acquired."
+        },
+        yearOfShow: Number,
+        MonthOfShow: Number,
+        dayOfShow: Number,
+        locationCity: String,
+        imageUrl: {
+            type: String,
+            default: 'https://i.imgur.com/xUNTDKM.jpg'
+        },
+        artist: String,
+        mediumType: String,
+        limtedEdition: Boolean,
+        band: String
+    }
+)
+
+const BandSchema = new Schema(
+    {
+        bandName: {
+            type: String,
+            required: [ true, 'The band name is required!'],
+            unique: true
+        },
+        hometown: String,
+        profile: String,
+        imageUrl: {
+            tyoe: String,
+            default: 'https://cdn.vectorstock.com/i/1000x1000/66/98/monkey-with-headphones-vector-6216698.jpg'
+        },
+        website: String,
+        yearFormed: Number
     }
 )
