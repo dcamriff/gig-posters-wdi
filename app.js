75881f9f587cd90b49015504e4a204f5f41b6e14
dcamriff <driffelatl@gmail.com>
2018-01-04 23:46:18 -0500

    added method-override requirement and app.use

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const index = require('./routes/indexController')
 const users = require('./routes/usersController')
@@ -20,36 +21,37 @@ mongoose.connection.once('open', () => {
 })
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(methodOverride('_method'))
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', index)
+app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500)
+  res.render('error')
 });
 
 // redirect to Users page on load
@@ -57,4 +59,4 @@ app.get('/', (req, res) => {
   res.redirect('/users')
 })
 
-module.exports = app;
+module.exports = app
