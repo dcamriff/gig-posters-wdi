9bfe52671752d170d0385b3f2a65c2078116200f
dcamriff <driffelatl@gmail.com>
2018-01-06 18:20:22 -0500

    added routes to postersController

--- routes/postersController.js
+++ routes/postersController.js
@@ -1,6 +1,83 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
-const Poster = require('../db/models/Poster')
+const User = require('../db/models/User')
 
+router.get('/', (req, res) => {
+    const userId = req.params.userId
+
+    User.findById(userId)
+    .then((user) => {
+        res.render('posters/index.hbs', {
+            userFullName: `${user.findName} ${user.lastName}`,
+            userId: user._id,
+            posters: user.posters,
+            pageTitle: 'Posters'
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+router.get('new', (req, res) => {
+    const userId = req.params.userId
+
+    res.render('posters/new.hbs', {
+        userId,
+        pageTitle: 'New_Poster'
+    })
+})
+
+router.get('/:storeId', (req, res) => {
+    const userId = req.params.userId
+    const posterId = req.params.posterId
+
+    User.findById(userId)
+    .then((user) => {
+        const poster = user.posters.id(posterId)
+        res.render('posters/show.hbs', {
+            userId,
+            poster,
+            pageTitle: 'Poster'
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+router.post('/', (req, res) => {
+    const userId = req.params.userId
+    const newStore = req.body
+
+    User.findById(userId)
+    .then((user) => {
+        user.posters.push(newPoster)
+        return user.save()
+    })
+    .then(() => {
+        res.redirect(`/users/${userId}/posters`)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+router.get('/:posterId/delete', (req, res) => {
+    const userId = req.params.userId
+    const storeId = req.params.storeId
+
+    User.findById(userId)
+    .then((user) => {
+        user.posters.id(posterId).remove()
+        return user.save()
+    })
+    .then(() => {
+        res.redirect(`/users/${userId}/posters/`)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
 
 module.exports = router
